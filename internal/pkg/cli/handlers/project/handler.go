9b2ef1a47b25ffd2eea1d81a0e9c2469473c8575
Isaac Garza <isaacgarza432@gmail.com>
2025-12-18 11:47:01 -0600

    refactor(project): extract version display logic to manager
    - Move version display methods from Handler to VersionDisplayManager
    - Add VersionDisplayManager field to Handler struct
    - Simplify Handler by delegating display logic to manager
    - Update version retrieval to use version.GetAppVersion()
    - Improve separation of concerns for version handling

--- internal/pkg/cli/handlers/project/handler.go
+++ internal/pkg/cli/handlers/project/handler.go
@@ -15,17 +15,19 @@ import (
 
 // Handler handles version command with update functionality
 type Handler struct {
-	enforcement *EnforcementHandler
-	output      *ui.Output
-	logger      *slog.Logger
+	enforcement           *EnforcementHandler
+	output                *ui.Output
+	logger                *slog.Logger
+	versionDisplayManager *VersionDisplayManager
 }
 
 // NewVersionHandler creates a new version handler
 func NewVersionHandler() *Handler {
 	return &Handler{
-		enforcement: NewEnforcementHandler(nil), // Can handle nil
-		output:      ui.NewOutput(),
-		logger:      logger.GetLogger(),
+		enforcement:           NewEnforcementHandler(nil), // Can handle nil
+		output:                ui.NewOutput(),
+		logger:                logger.GetLogger(),
+		versionDisplayManager: NewVersionDisplayManager(),
 	}
 }
 
@@ -61,7 +63,7 @@ func (h *Handler) handleCheckUpdates(_ context.Context, _ *cobra.Command, _ []st
 	h.output.Header("%s", core.MsgVersion_checking_updates)
 
 	// Get current version (this should come from build-time ldflags)
-	currentVersion := getCurrentVersion()
+	currentVersion := h.versionDisplayManager.GetCurrentVersion()
 	h.output.Info(core.MsgVersion_current_info, currentVersion)
 
 	// Check for updates
@@ -98,87 +100,23 @@ func (h *Handler) handleVersionDisplay(_ context.Context, cmd *cobra.Command, _ 
 		return err
 	}
 
-	currentVersion := getCurrentVersion()
+	currentVersion := h.versionDisplayManager.GetCurrentVersion()
 
 	if flags.Full {
-		return h.displayFullVersion(currentVersion, flags.Format)
+		return h.versionDisplayManager.DisplayFull(currentVersion, flags.Format)
 	}
 
-	return h.displayBasicVersion(currentVersion, flags.Format)
-}
-
-// displayBasicVersion displays basic version information
-func (h *Handler) displayBasicVersion(version, format string) error {
-	switch format {
-	case "json":
-		fmt.Printf(`{"version":"%s","app":"%s"}%s`, version, core.AppNameTitle, "\n")
-	case "yaml":
-		fmt.Printf("version: %s\napp: %s\n", version, core.AppNameTitle)
-	default:
-		fmt.Printf("%s version %s\n", core.AppNameTitle, version)
-	}
-	return nil
-}
-
-// displayFullVersion displays detailed version information
-func (h *Handler) displayFullVersion(version, format string) error {
-	buildInfo := getBuildInfo()
-	h.logger.Info("Displaying full version", logger.LogFieldVersion, version, logger.LogFieldFormat, format, logger.LogFieldBuildInfo, buildInfo)
-
-	switch format {
-	case "json":
-		fmt.Printf(`{"version":"%s","app":"%s","build":%s}%s`,
-			version, core.AppNameTitle, buildInfo.JSON(), "\n")
-	case "yaml":
-		fmt.Printf("version: %s\napp: %s\n%s", version, core.AppNameTitle, buildInfo.YAML())
-	default:
-		h.output.Header("%s Version Information", core.AppNameTitle)
-		h.output.Info(core.MsgVersion_version_label, version)
-		h.output.Info(core.MsgVersion_build_date, buildInfo.Date)
-		h.output.Info(core.MsgVersion_git_commit, buildInfo.Commit)
-		h.output.Info(core.MsgVersion_go_version, buildInfo.GoVersion)
-		h.output.Info(core.MsgVersion_platform, buildInfo.OS, buildInfo.Arch)
-	}
-	return nil
+	return h.versionDisplayManager.DisplayBasic(currentVersion, flags.Format)
 }
 
 // BuildInfo contains build-time information
-type BuildInfo struct {
-	Date      string `json:"date" yaml:"date"`
-	Commit    string `json:"commit" yaml:"commit"`
-	GoVersion string `json:"go_version" yaml:"go_version"`
-	OS        string `json:"os" yaml:"os"`
-	Arch      string `json:"arch" yaml:"arch"`
-}
 
 // JSON returns JSON representation
-func (b BuildInfo) JSON() string {
-	return fmt.Sprintf(`{"date":"%s","commit":"%s","go_version":"%s","os":"%s","arch":"%s"}`,
-		b.Date, b.Commit, b.GoVersion, b.OS, b.Arch)
-}
 
 // YAML returns YAML representation
-func (b BuildInfo) YAML() string {
-	return fmt.Sprintf("build:\n  date: %s\n  commit: %s\n  go_version: %s\n  os: %s\n  arch: %s\n",
-		b.Date, b.Commit, b.GoVersion, b.OS, b.Arch)
-}
 
 // Helper functions
 
 // getCurrentVersion returns the current version (set at build time)
-func getCurrentVersion() string {
-	// Use the main version package which has proper build-time injection
-	return version.GetShortVersion()
-}
 
 // getBuildInfo returns build information (set at build time)
-func getBuildInfo() BuildInfo {
-	mainBuildInfo := version.GetBuildInfo()
-	return BuildInfo{
-		Date:      mainBuildInfo.BuildDate,
-		Commit:    mainBuildInfo.GitCommit,
-		GoVersion: mainBuildInfo.GoVersion,
-		OS:        mainBuildInfo.Platform,
-		Arch:      mainBuildInfo.Arch,
-	}
-}
