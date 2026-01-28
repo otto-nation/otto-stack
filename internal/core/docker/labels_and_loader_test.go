bbd6ba2fc3709c261d4f653a594bf87614f38c73
Isaac Garza <isaacgarza432@gmail.com>
2026-01-28 13:10:34 -0600

    refactor: move codegen functions to cmd/codegen package
    - Move LoadCommandConfig and LoadCommandConfigStruct from internal/pkg/config to cmd/codegen
    - Move CommandConfig, Command, GlobalConfig, FlagConfig types to codegen
    - Update generate-registers to use codegen package
    - Remove tests for moved functions
    - Use production ListContainers in e2e tests instead of ListProjectContainers
    - Remove unused ListProjectContainers method
    - Reduces deadcode from 8 to 6 functions

--- internal/core/docker/labels_and_loader_test.go
+++ internal/core/docker/labels_and_loader_test.go
@@ -3,42 +3,8 @@
 package docker
 
 import (
-	"context"
 	"testing"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/otto-nation/otto-stack/test/testhelpers"
 )
-
-func TestClient_ListProjectContainers_Unit(t *testing.T) {
-	mockDocker := &testhelpers.MockDockerClient{
-		ContainerListFunc: func(ctx context.Context, options container.ListOptions) ([]types.Container, error) {
-			return []types.Container{
-				{
-					ID:    "c1",
-					Names: []string{"/test-project-web-1"},
-					Labels: map[string]string{
-						"com.docker.compose.project": "test-project",
-					},
-				},
-			}, nil
-		},
-	}
-
-	client := NewClientWithDependencies(mockDocker, nil, testhelpers.MockLogger())
-
-	ctx := context.Background()
-	containers, err := client.ListProjectContainers(ctx, "test-project")
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	if len(containers) != 1 {
-		t.Errorf("Expected 1 container, got %d", len(containers))
-	}
-}
 
 func TestNewDefaultProjectLoader(t *testing.T) {
 	loader, err := NewDefaultProjectLoader()
